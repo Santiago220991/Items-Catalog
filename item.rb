b9d9abbff172c82ca9dc2989c084678cc7c08560
unknown <skj99667788@gmail.com>
2022-08-02 01:21:58 +0500

    Create move_to_archieve method

--- item.rb
+++ item.rb
@@ -12,7 +12,7 @@ class Item
     @source = source
     @label = label
     @publish_date = publish_date
-    @archived = archived
+    @archived = false
   end
 
   def can_be_archived?
@@ -20,5 +20,6 @@ class Item
   end
 
   def move_to_archive
+    @archived = true if can_be_archived?
   end
 end
