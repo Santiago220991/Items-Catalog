4a7a48f2045bca8d2b79e7b7f4788f8345b0811a
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-08-01 17:15:49 -0300

    create item class with "can_be_archived?" method

---
+++ item.rb
@@ -0,0 +1,24 @@
+require 'date'
+
+class Item
+  attr_accessor :genre, :author, :source, :label
+
+  attr_reader :id, :publish_date
+  
+  def initialize(publish_date, archived)
+    @id = Random.rand(1..100)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def can_be_archived?
+    Date.today.year - DateTime.parse(@publish_date).year > 10
+  end
+
+  def move_to_archive
+  end
+end
