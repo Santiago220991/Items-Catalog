4f777573a451b7e0bcf30fd5999e75bde1a6888f
Santiago <alexander.cardenas10@outlook.com>
2022-08-01 16:39:41 -0500

    Change archived argument as default

--- item.rb
+++ item.rb
@@ -5,14 +5,14 @@ class Item
 
   attr_reader :id, :publish_date
 
-  def initialize(publish_date, archived)
+  def initialize(publish_date, archived=false)
     @id = Random.rand(1..100)
     @genre = genre
     @author = author
     @source = source
     @label = label
     @publish_date = publish_date
-    @archived = false
+    @archived = archived
   end
 
   def move_to_archive
