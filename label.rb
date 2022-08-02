c39e9ac53c4b4510451fef68398421972e597a85
Santiago <alexander.cardenas10@outlook.com>
2022-08-02 14:26:37 -0500

    Add method to preserve label data

--- label.rb
+++ label.rb
@@ -14,4 +14,16 @@ class Label
     @items << item
     item.label = self
   end
+
+  def to_json(*args)
+    {
+    JSON.create_id => self.class.name, 'a' => [@id, @title, @color]
+    }.to_json(*args)
+    end
+    
+    def self.json_create(object)
+    label = new(title: object['a'][1], color: object['a'][2])
+    label.id = object['a'][0]
+    label
+    end
 end
