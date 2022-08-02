d0a193b123fffad39e3a9ccc8f11d36dd915215c
unknown <skj99667788@gmail.com>
2022-08-03 02:13:08 +0500

    Rubocup error solved

--- music_album/musicui.rb
+++ music_album/musicui.rb
@@ -3,7 +3,7 @@ require './music_album/genre'
 
 module Music
   def create_music
-    publish_date =  [(print 'Insert publish date '), gets.rstrip][1]
+    publish_date = [(print 'Insert publish date '), gets.rstrip][1]
     puts('Is on spotify? [Y/N]: ')
     response = gets.chomp
     on_spotify = response.downcase == 'y'
