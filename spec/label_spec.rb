ed6b0d38b864a3b5b5b0e925b664b78eb992e44d
Santiago <alexander.cardenas10@outlook.com>
2022-08-02 10:20:10 -0500

    Check linters ready to pull request

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,28 +1,25 @@
 require './label'
 
 describe Label do
-context "Testing Label class" do
-it "An instance of Label should be created" do
-label=Label.new(title: "title", color: "color")
-expect(label).to be_instance_of(Label)
-end
+  context 'Testing Label class' do
+    it 'An instance of Label should be created' do
+      label = Label.new(title: 'title', color: 'color')
+      expect(label).to be_instance_of(Label)
+    end
 
-it "Should contain the appropriate instance variable title, color, items" do
-label=Label.new(title: "title", color: "color")
-expect(label.title).to eq("title")
-expect(label.color).to eq("color")
-expect(label.items).to eq([])
-end
+    it 'Should contain the appropriate instance variable title, color, items' do
+      label = Label.new(title: 'title', color: 'color')
+      expect(label.title).to eq('title')
+      expect(label.color).to eq('color')
+      expect(label.items).to eq([])
+    end
 
-it "add_item method should add an item to items property" do
-label=Label.new(title: "title", color: "color")
-book=double("book")
-allow(book).to receive(:label=){[]}
-label.add_item(book)
-expect(label.items.length).to eq(1)
+    it 'add_item method should add an item to items property' do
+      label = Label.new(title: 'title', color: 'color')
+      book = double('book')
+      allow(book).to receive(:label=) { [] }
+      label.add_item(book)
+      expect(label.items.length).to eq(1)
+    end
+  end
 end
-end
-end
-
-
-
