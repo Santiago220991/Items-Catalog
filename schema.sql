7eb23f54b3cdd63065df2dfa57c94a5be183db3f
unknown <skj99667788@gmail.com>
2022-08-04 01:14:18 +0500

    required changes

--- schema.sql
+++ schema.sql
@@ -2,6 +2,11 @@ CREATE DATABASE Items_catalog;
 
 DROP TABLE IF EXISTS genre;
 DROP TABLE IF EXISTS music_album;
+DROP TABLE IF EXISTS Label;
+DROP TABLE IF EXISTS Author;
+DROP TABLE IF EXISTS Game;
+DROP TABLE IF EXISTS Book;
+DROP TABLE IF EXISTS Item;
 
 CREATE TABLE Label (
     id INT GENERATED ALWAYS AS IDENTITY,
@@ -21,21 +26,22 @@ CREATE TABLE Author (
 
 CREATE TABLE genre(
 	id SERIAL PRIMARY KEY,
-	name varchar(100) NULL
+	name varchar(100) NULL,
+    items INT ,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE Item (
     id INT GENERATED ALWAYS AS IDENTITY,
     label INT,
-    genre_id int NULL,
+    genre_id int NOT NULL,
     author INT,
     publish_date DATE NOT NULL,
     archived BOOLEAN NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (label) REFERENCES Label (id),
     FOREIGN KEY (author) REFERENCES Author (id),
-    FOREIGN KEY(genre_id) REFERENCES genre (id),
-    FOREIGN KEY(id) REFERENCES music_album (id);
+    FOREIGN KEY(genre_id) REFERENCES genre (id)
 );
 
 CREATE TABLE Book ( 
@@ -58,5 +64,8 @@ CREATE TABLE Game (
 
 CREATE TABLE music_album(
 	id SERIAL PRIMARY KEY,
-	on_spotify boolean NULL
+	on_spotify boolean NOT NULL,
+    item_id INT NOT NULL, 
+    FOREIGN KEY (item_id) REFERENCES Item (id), 
+    PRIMARY KEY (id) 
 );
