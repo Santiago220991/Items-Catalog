09e62117501b0c813c5bfb9770a83dd65c224bff
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-08-03 13:18:44 -0300

    create Author table and a FOREIGN KEY in iiem table

--- schema.sql
+++ schema.sql
@@ -7,13 +7,23 @@ CREATE TABLE Label (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE Author (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    items INT ,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE Item (
     id INT GENERATED ALWAYS AS IDENTITY,
     label INT,
+    author INT
     publish_date DATE NOT NULL,
     archived BOOLEAN NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (label) REFERENCES Label (id)
+    FOREIGN KEY (label) REFERENCES Label (id),
+    FOREIGN KEY (author) REFERENCES Author (id)
 );
 
 CREATE TABLE Book ( 
@@ -23,4 +33,4 @@ CREATE TABLE Book (
     cover_state TEXT NOT NULL, 
     FOREIGN KEY (item_id) REFERENCES Item (id), 
     PRIMARY KEY (id) 
-); 
+);
