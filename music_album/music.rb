54a569187981396ec188935b1017987f60dbd8fc
Santiago <alexander.cardenas10@outlook.com>
2022-08-04 19:12:48 -0500

    Add label and author properties to music album

--- music_album/music.rb
+++ music_album/music.rb
@@ -10,13 +10,13 @@ class MusicAlbum < Item
 
   def to_json(*args)
     {
-      JSON.create_id => self.class.name, 'props' => [@id, @genre, @on_spotify, @publish_date]
+      JSON.create_id => self.class.name, 'props' => [@id, @genre, @on_spotify, @publish_date, @label, @author]
     }.to_json(*args)
   end
 
   def self.json_create(object)
     { id: object['props'][0], genre: object['props'][1], on_spotify: object['props'][2],
-      publish_date: object['props'][3] }
+      publish_date: object['props'][3], label: object['props'][4], author: object['props'][5] }
   end
 
   def can_be_archived?
--- music_album/musicui.rb
+++ music_album/musicui.rb
@@ -1,17 +1,24 @@
 require './music_album/music'
 require './music_album/genre'
+require './books/label'
+require './commom_ui'
+require './games/author'
 
 module Music
+  include CommonUI
   def create_music
-    publish_date = [(print 'Insert publish date: '), gets.rstrip][1]
+    props=common_properties
     response = [(print 'Is on spotify? [Y/N]: '), gets.rstrip][1]
     on_spotify = response.downcase == 'y'
-    name = [(print 'Insert Name: '), gets.rstrip][1]
-    music_album = MusicAlbum.new(publish_date: publish_date, on_spotify: on_spotify)
-    genre = Genre.new(name: name)
+    music_album = MusicAlbum.new(publish_date: props[:publish_date], on_spotify: on_spotify)
+    genre = Genre.new(name: props[:genre_name])
+    label = Label.new(title: props[:title], color: props[:color])
+    author = Author.new(first_name: props[:author_first], last_name: props[:author_last])
     genre.add_item(music_album)
+    label.add_item(music_album)
+    author.add_item(music_album)
     puts ''
     puts 'music album created successfully'
-    { music_album: music_album, genre: genre }
+    { music_album: music_album, genre: genre , label: label, author: author}
   end
 end
